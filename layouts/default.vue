560aa0cf1173f869da34335a5d9e593b3be76483
noaione <noaione0809@gmail.com>
2023-11-14 15:52:56 +0700

    feat: add nav and app bar for default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,47 @@
 <template>
-  <VMain>
-    <slot />
-  </VMain>
+  <div class="fill-height">
+    <VAppBar scroll-behavior="elevate" scroll-threshold="0" order="1">
+      <VBadge dot :model-value="showBadge" :color="info.booksToCheck > 0 ? 'accent' : 'warning'" class="ms-2">
+        <VAppBarNavIcon @click.stop="drawerVisible = !drawerVisible" />
+      </VBadge>
+
+      <SearchBox class="flex-fill" />
+    </VAppBar>
+
+    <NavigationDrawer v-model="drawerVisible" />
+
+    <VMain class="fill-height">
+      <slot />
+    </VMain>
+  </div>
 </template>
+
+<script setup lang="ts">
+import SearchBox from "~/composables/Search/SearchBox.vue";
+
+const display = useDisplay();
+const info = useKomgaGlobals();
+const drawerVisible = ref(display.lgAndUp);
+
+const showBadge = computed(() => {
+  return !drawerVisible && (info.booksToCheck > 0 || info.announcementCount > 0);
+});
+
+async function checkForAnnouncement() {
+  const { origin } = useKomgaServerUrl();
+
+  const { data } = await useKomgaFetch("/api/v1/announcements", {
+    method: "GET",
+    credentials: "include",
+    baseURL: origin,
+  });
+
+  if (data.value) {
+    info.setAnnuncements(data.value.items);
+  }
+}
+
+onMounted(async () => {
+  await checkForAnnouncement();
+});
+</script>
