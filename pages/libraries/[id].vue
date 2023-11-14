de2790d5abd1f3c7e7302c29a65139d9b5dd8c33
noaione <noaione0809@gmail.com>
2023-11-14 19:11:15 +0700

    fix: routing check when changing

--- pages/libraries/[id].vue
+++ pages/libraries/[id].vue
@@ -2,25 +2,20 @@
   <div :style="$vuetify.display.xs ? { marginBottom: '56px' } : undefined">
     <ToolbarSticky>
       <MenusLibraryAction v-if="library.library" :library="library.library" />
-
       <VToolbarTitle>
         <span>{{ library.library?.name ?? $t("common.all_libraries") }}</span>
         <VChip v-if="upstream.toolbarCount" label class="mx-4">
           <span :style="{ fontSize: '1.1rem' }">{{ upstream.toolbarCount }}</span>
         </VChip>
       </VToolbarTitle>
-
       <LibraryNavigation v-if="$vuetify.display.mdAndUp" :library-id="library.libraryId" />
       <VSpacer />
-
       <MenusPageSize
         v-if="!$route.path.endsWith('/recommended') || libraryId === 'all'"
         v-model="config.pageSize.libraries"
       />
     </ToolbarSticky>
-
     <LibraryNavigation v-if="$vuetify.display.smAndDown" :library-id="library.libraryId" bottom-navigation />
-
     <NuxtPage v-if="library.libraryId !== 'all'" />
   </div>
 </template>
@@ -49,11 +44,24 @@ async function processRoute(routeId: string) {
 }
 
 watch(
-  () => route.params?.id,
-  async (routeId) => {
-    if (routeId) {
-      await processRoute(String(routeId));
+  () => [route.path, route.params?.id],
+  async ([routeFull, routeId]) => {
+    if (String(routeFull).includes("/all") && !String(routeFull).startsWith("/libraries/all")) {
+      await processRoute("all");
+
+      return;
     }
+
+    const paramId = String(routeId);
+
+    if (String(routeFull).endsWith(paramId)) {
+      console.log("Forcing route for", paramId);
+
+      await processRoute(paramId);
+    }
+  },
+  {
+    immediate: true,
   }
 );
 
